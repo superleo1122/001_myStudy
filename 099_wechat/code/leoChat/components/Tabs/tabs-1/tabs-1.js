87b106b649035b6105bfe1d6935ac2e010ece293
廖友 <you.liao@lanmaoly.com>
2020-07-14 23:55:25 +0800

    暂存

---
+++ 099_wechat/code/leoChat/components/Tabs/tabs-1/tabs-1.js
@@ -0,0 +1,30 @@
+// components/Tabs/tabs-1/tabs-1.js
+Component({
+    /**
+   * 组件的属性列表
+   */
+    properties: {
+        tabList: {
+            type: Array,
+            value: ['sport', 'food', 'gold']
+        },
+        currentIndex: {
+            type: Number,
+            value: 0
+        }
+    },
+
+    /**
+   * 组件的初始数据
+   */
+    data: {
+
+    },
+
+    /**
+   * 组件的方法列表
+   */
+    methods: {
+
+    }
+});
